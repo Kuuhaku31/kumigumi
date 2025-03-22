48976eeb80331374c7f62bfd80d37ea8f9d3f1ca
Kuuhaku_kazari <SP_YYY@outlook.com>
2025-03-22 22:51:21 +0800

    重构爬取功能，优化 HTML 解析逻辑，新增单集信息提取，更新 CSV 文件处理

--- src/python/bangumi/ba.py
+++ src/python/bangumi/ba.py
@@ -5,28 +5,35 @@ import urllib.request  # noqa: E402
 
 from lxml import etree
 
-headers = ["url", "名称", "中文名", "别名", "话数", "放送开始", "放送星期", "官方网站", "封面"]
+headers = ["url", "名称", "中文名", "别名", "话数", "放送开始", "放送星期", "官方网站", "封面", "话列表"]
 
 
-# 解析番剧信息
-def prase_anime_info(url):
+# 获取html页面
+def get_html(url: str) -> str:
+    print(f"正在请求 {url}")
 
-    print(f"正在解析 {url}")
-
-    # 请求
     res = urllib.request.urlopen(url)
     if res.status != 200:
         print(f"请求失败：{url}")
+        return None
+    else:
+        print(f"请求成功：{url}")
+        return res.read().decode("utf-8")
 
-    tree = etree.HTML(res.read().decode("utf-8"))
+
+# 解析番剧信息
+def prase_html(html: str) -> dict:
+    tree = etree.HTML(html)
 
     # 初始化字典
     anime_infos = {}
     for header in headers:
         anime_infos[header] = ""
-    anime_infos["url"] = url
+    anime_infos["url"] = "https://bangumi.tv" + tree.xpath("//h1/a/@href")[0]
     anime_infos["名称"] = tree.xpath("//h1/a/text()")[0]
     anime_infos["中文名"] = tree.xpath("//h1/a/text()")[0]
+
+    print(f"正在解析 {anime_infos['名称']}")
 
     # 查找 ul#infobox 内的所有 li
     infobox_lis = tree.xpath('//ul[@id="infobox"]/li')
@@ -63,19 +70,60 @@ def prase_anime_info(url):
     if cover:
         anime_infos["封面"] = "https:" + cover[0]
 
+    # 获取单集信息
+    prg_list = []
+    prg_lis = tree.xpath('//ul[@class="prg_list"]/li')
+    for prg in prg_lis:
+        new_prg = {}
+        new_prg["话索引"] = prg.xpath(".//a/text()")[0]
+        new_prg["话标题"] = prg.xpath(".//a/@title")[0].split(" ", 1)[1]  # 按第一个空格分割，取第二部分
+
+        id = prg.xpath(".//a/@rel")[0].replace("#", "")
+        span = tree.xpath(f'//div[@id="{id}"]/span')
+        tip_text = (
+            etree.tostring(span[0], encoding="utf-8")
+            .decode("utf-8")
+            .replace('<span class="tip">', "")
+            .replace("<span>", "")
+            .replace("</span>", "")
+            .replace(" ", "")
+            .replace("\n", "")
+            .strip()
+        )
+
+        # tips
+        for line in tip_text.split("<br/>"):
+            if ":" in line:
+                key, value = line.split(":", 1)
+                new_prg[key.strip()] = value.strip()
+
+        prg_list.append(new_prg)
+
+    anime_infos["话列表"] = prg_list
+
     print(f"解析完成：{anime_infos['名称']}")
 
     return anime_infos
 
 
+def prase_anime_info(url):
+
+    html = get_html(url)
+    if not html:
+        return None
+
+    anime_info = prase_html(html)
+    return anime_info
+
+
 # 根据url在线更新 csv 文件
-def update_csv(file_name):
+def update_csv(json_file: str, csv_file: str):
 
     data = []
     new_data = []
 
     # 读取 json 文件
-    with open(file_name, "r", encoding="utf-8") as f:
+    with open(json_file, "r", encoding="utf-8") as f:
         data = json.load(f)
 
     # 遍历csv每一行
@@ -84,7 +132,7 @@ def update_csv(file_name):
         new_data.append(new_info)
 
     # 保存到 csv 文件
-    with open("./data.csv", "w+", newline="", encoding="utf-8") as f:
+    with open(csv_file, "w+", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
         writer.writerow(headers)
         for row in new_data:
--- src/python/main.py
+++ src/python/main.py
@@ -1,7 +1,34 @@
+import csv
+
 from bangumi import ba
 
-url = "https://bangumi.tv/subject/524801"
 
-print("1")
-res = ba.prase_anime_info(url)
-print(res)
+def f1():
+    url = "https://bangumi.tv/subject/389156"
+
+    print("test")
+    html = ba.get_html(url)
+
+    with open("data/test.html", "w", encoding="utf-8") as f:
+        f.write(html)
+
+
+def f2():
+    res = None
+
+    with open("data/test.html", "r", encoding="utf-8") as f:
+        html = f.read()
+        res = ba.prase_html(html)
+
+    for key, value in res.items():
+        print(f"{key}: {value}")
+
+    with open("data/data.csv", "w+", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(ba.headers)
+        writer.writerow(res.values())
+
+
+# f1()
+# f2()
+ba.update_csv("data/urls.json", "data/data.csv")
