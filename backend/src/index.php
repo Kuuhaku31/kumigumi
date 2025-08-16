732f614867931161bf7f53ca757f743d6d7f1ed1
kuuhaku-zks <147358727+Kuuhaku31@users.noreply.github.com>
2025-08-16 21:47:36 +0800

    更新数据库连接配置，移除MYSQL_ROOT_PASSWORD环境变量，添加MYSQL_HOST变量

--- backend/src/index.php
+++ backend/src/index.php
@@ -1,15 +1,21 @@
 <?php
 
-$MYSQL_ROOT_PASSWORD = getenv('MYSQL_ROOT_PASSWORD');
+$MYSQL_HOST = getenv('MYSQL_HOST');
 $MYSQL_DATABASE = getenv('MYSQL_DATABASE');
 $MYSQL_USER = getenv('MYSQL_USER');
 $MYSQL_PASSWORD = getenv('MYSQL_PASSWORD');
 
 $text = "<h1>hello</h1>";
-$text .= "MYSQL_ROOT_PASSWORD: $MYSQL_ROOT_PASSWORD<br>";
 $text .= "MYSQL_DATABASE: $MYSQL_DATABASE<br>";
 $text .= "MYSQL_USER: $MYSQL_USER<br>";
 $text .= "MYSQL_PASSWORD: $MYSQL_PASSWORD<br>";
+
+$dsn = "mysql:host=$MYSQL_HOST;dbname=$MYSQL_DATABASE;charset=utf8mb4";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
 
 print_r($text);
 
@@ -20,3 +26,21 @@ $config = json_decode($jsonString, true);
 echo "<hr>";
 echo "<h2>Config Data</h2>";
 print_r($config);
+echo "<hr>";
+
+// 链接到数据库
+try {
+    $pdo = new PDO($dsn, $MYSQL_USER, $MYSQL_PASSWORD, $options);
+    echo "<p>数据库连接成功</p>";
+
+    // 获取所有数据库列表
+    $stmt = $pdo->query("SHOW DATABASES");
+    // while ($row = $stmt->fetch()) {
+    //     $list[] = $row[0];
+    // }
+    echo "<h2>Databases</h2>";
+    print_r($stmt->fetchAll(PDO::FETCH_COLUMN));
+} catch (PDOException $e) {
+    echo "数据库连接失败: " . $e->getMessage();
+    exit;
+}
