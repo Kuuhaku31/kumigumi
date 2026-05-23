b209ae40fb77055fecd0ff3f438c0efda0e8fcab
kuuhaku@hanna <kuuhaku-kzr@outlook.com>
2026-05-23 17:26:01 +0900

    更新 .gitignore，添加对 .db 文件的忽略；新增 TestInitDB 类用于初始化测试数据库；更新 SQLiteInit.java，添加新表结构以支持 RSS 和种子记录

--- src/java/main/Database/SQLiteInit.java
+++ src/java/main/Database/SQLiteInit.java
@@ -3,109 +3,129 @@ package Database;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 class SQLiteInit {
 
     private static final String sqlCreateAnimeTable =
-        """
-        CREATE TABLE "anime" (
+    """
+    CREATE TABLE "anime" (
 
-        "ANI_ID"            integer NOT NULL,
+    "ANI_ID"            integer NOT NULL,
 
-        "air_date"          text,
-        "title"             text,
-        "title_cn"          text,
-        "aliases"           text,
-        "description"       text,
-        "episode_count"     integer,
-        "url_official_site" text,
-        "url_cover"         text,
+    "air_date"          text,
+    "title"             text,
+    "title_cn"          text,
+    "aliases"           text,
+    "description"       text,
+    "episode_count"     integer,
+    "url_official_site" text,
+    "url_cover"         text,
 
-        "url_rss"           text,
-        "rating_before"     integer,
-        "rating_after"      integer,
-        "remark"            text,
+    "update_datetime" text NOT NULL,
 
-        PRIMARY KEY ("ANI_ID" DESC)
-        );
-        """;
+    PRIMARY KEY ("ANI_ID" DESC)
+    );
+    """;
 
     private static final String sqlCreateEpisodeTable =
-        """
-        CREATE TABLE "episode" (
+    """
+    CREATE TABLE "episode" (
 
-        "EPI_ID"          integer NOT NULL,
-        "ANI_ID"          integer NOT NULL,
+    "EPI_ID"          integer NOT NULL,
+    "ANI_ID"          integer NOT NULL,
 
-        "ep"              text,
-        "sort"            real,
-        "air_date"        text,
-        "duration"        integer,
-        "title"           text,
-        "title_cn"        text,
-        "description"     text,
+    "ep"              text,
+    "sort"            real,
+    "air_date"        text,
+    "duration"        integer,
+    "title"           text,
+    "title_cn"        text,
+    "description"     text,
 
-        "rating"          integer,
-        "view_datetime"   text,
-        "status_download" text,
-        "status_view"     text,
-        "remark"          text,
+    "update_datetime" text NOT NULL,
 
-        PRIMARY KEY ("EPI_ID" DESC),
+    PRIMARY KEY ("EPI_ID" DESC),
 
-        CONSTRAINT "ANI_ID"
-        FOREIGN KEY ("ANI_ID") 
-        REFERENCES "anime" ("ANI_ID") 
-        ON DELETE CASCADE ON UPDATE CASCADE
-        );
-        """;
+    CONSTRAINT "ANI_ID"
+    FOREIGN KEY ("ANI_ID") 
+    REFERENCES "anime" ("ANI_ID")
+    ON DELETE CASCADE ON UPDATE CASCADE
+    );
+    """;
 
     private static final String sqlCreateTorrentTable =
-        """
-        CREATE TABLE "torrent" (
+    """
+    CREATE TABLE "torrent" (
 
-        "TOR_HASH"  text NOT NULL,
+    "TOR_HASH"     text NOT NULL,
 
-        "file_name"    text,
-        "file_size"    integer,
-        "torrent_file" blob,
+    "file_name"    text,
+    "file_size"    integer,
+    "torrent_file" blob,
 
-        "remark"    text,
-        
-        PRIMARY KEY ("TOR_HASH" DESC)
-        );
-        """;
+    PRIMARY KEY ("TOR_HASH" DESC)
+    );
+    """;
 
-    private static final String sqlCreateAniTorTable =
-        """
-        CREATE TABLE "ani_tor" (
+    private static final String sqlCreateTorrentPageTable =
+    """
+    CREATE TABLE "torrent_page" (
 
-        "ANI_ID"          integer NOT NULL,
-        "TOR_HASH"        text    NOT NULL,
+    "URL_RSS"         text NOT NULL,
+    "TOR_HASH"        text NOT NULL,
 
-        "air_datetime"    text,
-        "url_download"    text,
-        "url_page"        text,
-        "title"           text,
-        "subtitle_group"  text,
-        "description"     text,
+    "air_datetime"    text,
+    "url_download"    text,
+    "url_page"        text,
+    "title"           text,
+    "subtitle_group"  text,
+    "description"     text,
 
-        "status_download" text,
-        "remark"          text,
-        
-        PRIMARY KEY ("ANI_ID" DESC, "TOR_HASH" DESC),
-        
-        CONSTRAINT "ANI_ID" 
-        FOREIGN KEY ("ANI_ID") 
-        REFERENCES "anime" ("ANI_ID")
-        ON DELETE CASCADE ON UPDATE CASCADE,
+    "update_datetime" text NOT NULL,
 
-        CONSTRAINT "TOR_HASH" 
-        FOREIGN KEY ("TOR_HASH") 
-        REFERENCES "torrent" ("TOR_HASH") 
-        ON DELETE CASCADE ON UPDATE CASCADE
-        );
-        """;
+    PRIMARY KEY ("URL_RSS" DESC, "TOR_HASH" DESC),
 
+    CONSTRAINT "URL_RSS"
+    FOREIGN KEY ("URL_RSS")
+    REFERENCES "rss" ("URL_RSS")
+    ON DELETE CASCADE ON UPDATE CASCADE
+    );
+    """;
+
+    private static final String sqlCreateRSSTable =
+    """
+    CREATE TABLE "rss" (
+
+    "URL_RSS" text NOT NULL,
+    "ANI_ID"  integer,
+
+    PRIMARY KEY ("URL_RSS" DESC),
+
+    CONSTRAINT "ANI_ID"
+    FOREIGN KEY ("ANI_ID")
+    REFERENCES "anime" ("ANI_ID")
+    ON DELETE SET NULL ON UPDATE CASCADE
+    );
+    """;
+
+    private static final String sqlCreateEpisodeRecordTable =
+    """
+    CREATE TABLE "episode_record" (
+
+    "EPI_ID"        integer NOT NULL,
+    "view_datetime" text NOT NULL,
+
+    "rating"  integer,
+    "comment" text,
+
+    PRIMARY KEY ("EPI_ID" DESC, "view_datetime" DESC),
+
+    CONSTRAINT "EPI_ID"
+    FOREIGN KEY ("EPI_ID")
+    REFERENCES "episode" ("EPI_ID")
+    ON DELETE CASCADE ON UPDATE CASCADE
+    );
+    """;
 
     /**
      * 初始化数据库，创建表格
@@ -116,7 +136,9 @@ class SQLiteInit {
             conn.prepareStatement(sqlCreateAnimeTable).execute();
             conn.prepareStatement(sqlCreateEpisodeTable).execute();
             conn.prepareStatement(sqlCreateTorrentTable).execute();
-            conn.prepareStatement(sqlCreateAniTorTable).execute();
+            conn.prepareStatement(sqlCreateTorrentPageTable).execute();
+            conn.prepareStatement(sqlCreateRSSTable).execute();
+            conn.prepareStatement(sqlCreateEpisodeRecordTable).execute();
         }
         System.out.println("数据库创建完成");
     }
